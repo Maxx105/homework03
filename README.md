976cd3b13a953e8ce9557e6751f5e5abac710e3b
Maxx Sanner <maxxsanner105@gmail.com>
2020-06-25 16:18:30 -0700

    added details to readme and added some more notes within the script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # homework03
+
 JavaScript: Password Generator
+
+This was the third homework assignment for the full stack web development program at UC Irvine, in which the goal was to build a password generator using javascript.
+
+How to Use:
+When the user clicks the Generate Password button, it then prompts them with 5 criteria for the generated password:
+1) Password length (must be between 8 and 128 characters)
+2) lowercase letters
+3) uppercase letters
+4) numbers
+5) special characters
+
+It essentially takes a string of all of the possible characters (lowercase, uppercase, numbers, and special characters) and then pulls from that to create a new string based on the answers to the above criteria. It then pulls a random character from this new string and adds it to each index of an array (called genPassword) from i = 0 to i < the number of characters chosen in the first prompt, using a for loop. Lastly, it converts the genPassword array back to a string and returns this.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
 //This sets the length of the password
-var numberChar = parseInt(prompt("Choose password length"));
+var numberChar = parseInt(prompt("Choose password length (must be between 8 and 128"));
 
 //These for confirm functions determine which characters the password can have.
 var lowerCase = confirm("Do you want lowercase letters?");
@@ -18,7 +18,7 @@ var charArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!
 //this string will change depending on what type of characters the user chooses.
 var passwordArray = ""
 
-//This logic basically changes the passwordArray (string) depending on which options are chosen.
+//This logic basically changes the passwordArray (string) depending on which options are chosen using the substring function to pull different sections of the charArray string.
 
   if (lowerCase) {
     var passwordArray = charArray.substring(0,26);
@@ -39,8 +39,10 @@ var passwordArray = ""
   else if (numberChar >= 8 || numberChar <= 128) {
 
       //this array is the generated password (as an array) using the characters that the user chooses.
+
       var genPassword = [];
 
+      //This for loop creates a random number and sets the genPassword array to have as many items as there are characters chosen for the password. It then sets each index of this array to a different random character from the passwordArray string.
       for (var i = 0; i < numberChar; i++){
         var randomNum = Math.floor(Math.random() * passwordArray.length);
         genPassword.push(i);
