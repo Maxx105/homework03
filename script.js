5e95cb29f0bf649b4d3a5b9bfee6ba6840c3cec5
Maxx Sanner <maxxsanner105@gmail.com>
2020-06-25 12:14:57 -0700

    added script to generate password

--- script.js
+++ script.js
@@ -3,53 +3,59 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
+//This sets the length of the password
 var numberChar = parseInt(prompt("Choose password length"));
+
+//These for confirm functions determine which characters the password can have.
 var lowerCase = confirm("Do you want lowercase letters?");
 var upperCase = confirm("Do you want uppercase letters?");
 var numeric = confirm("Do you want numbers?");
 var specialChar = confirm("Do you want special characters?");
-//this is a string of ALL the possible characters that could be in the password.
+
+//this is a string of ALL the possible characters that could be in the password (includes lowercase letters, uppercase letters, numbers, and special characters).
 var charArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
 //this string will change depending on what type of characters the user chooses.
 var passwordArray = ""
-//this string is the generated password using the characters that the user chooses.
-var genPassword = ""
+
+//This logic basically changes the passwordArray (string) depending on which options are chosen.
 
   if (lowerCase) {
     var passwordArray = charArray.substring(0,26);
-    //console.log(passwordArray);
   }
   if (upperCase) {
     var passwordArray = (passwordArray + charArray.substring(26,52));
-    //console.log(passwordArray);
   }
   if (numeric) {
     var passwordArray = (passwordArray + charArray.substring(52,62));
-    //console.log(passwordArray);
   }
   if (specialChar) {
     var passwordArray = (passwordArray + charArray.substring(62,charArray.length));
-    //console.log(passwordArray);
   }
+
   if (numberChar < 8 || numberChar > 128) {
     alert("Must be between 8 and 128 characters!");
   }
-  else {
-      for (var i = 0; i < numberChar; i++){
-        var randomNum = Math.floor(Math.random() * passwordArray.length) + 1;
-        genPassword.length = numberChar;
-        for (var j = 0; j < genPassword.length; j++){
-          genPassword[i] = genPassword + passwordArray[randomNum];
-        }
-       // genPassword = genPassword + passwordArray[randomNum];
-        //genPassword[2] = genPassword + passwordArray[randomNum];
-        
-      
-        return genPassword;
-    }
-    
-  }
+  else if (numberChar >= 8 || numberChar <= 128) {
 
+      //this array is the generated password (as an array) using the characters that the user chooses.
+      var genPassword = [];
+
+      for (var i = 0; i < numberChar; i++){
+        var randomNum = Math.floor(Math.random() * passwordArray.length);
+        genPassword.push(i);
+        genPassword[i] = passwordArray[randomNum];
+
+        //this variable turns out to be the final generated password. The join function converts the genPassword array into a string separated by nothing.
+        var finalPassword = genPassword.join('');
+      }
+
+      return finalPassword;
+
+  }
+  else {
+    alert("Must enter a number!");
+  }
 }
 
 // Write password to the #password input
@@ -62,5 +68,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
 
