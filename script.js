46d276140ef685eebf5e507d03386c3f8f565209
Maxx Sanner <maxxsanner105@gmail.com>
2020-06-26 09:49:59 -0700

    fixed typo and slightly changed order of variables

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ function generatePassword() {
 //This sets the length of the password
 var numberChar = parseInt(prompt("Choose password length (must be between 8 and 128)"));
 
-//These for confirm functions determine which characters the password can have.
+//These four confirm functions determine which characters the password can have.
 var lowerCase = confirm("Do you want lowercase letters?");
 var upperCase = confirm("Do you want uppercase letters?");
 var numeric = confirm("Do you want numbers?");
 var specialChar = confirm("Do you want special characters?");
 
-//this is a string of ALL the possible characters that could be in the password (includes lowercase letters, uppercase letters, numbers, and special characters).
+//This is a string of ALL the possible characters that could be in the password (includes lowercase letters, uppercase letters, numbers, and special characters).
 var charArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-//this string will change depending on what type of characters the user chooses.
+//This string will change depending on what type of characters the user chooses.
 var passwordArray = ""
+
+//This array is the generated password (as an array) using the characters that the user chooses.
+var genPassword = [];
 
 //This logic basically changes the passwordArray (string) depending on which options are chosen using the substring function to pull different sections of the charArray string.
 
@@ -41,10 +44,6 @@ var passwordArray = ""
     alert("Must be between 8 and 128 characters!");
   }
   else if (numberChar >= 8 || numberChar <= 128) {
-
-      //this array is the generated password (as an array) using the characters that the user chooses.
-
-      var genPassword = [];
 
       //This for loop creates a random number and sets the genPassword array to have as many items as there are characters chosen for the password. It then sets each index of this array to a different random character from the passwordArray string.
       for (var i = 0; i < numberChar; i++){
